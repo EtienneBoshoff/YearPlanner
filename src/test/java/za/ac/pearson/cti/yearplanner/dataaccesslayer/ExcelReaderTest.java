f28b4890e955b8a24375918a6424e4da4e8ee1ec
EtienneBoshoff <boshoff.etienne@gmail.com>
2014-11-19 17:33:51 +0200

    renamed test package to conform to standards

--- src/test/java/za/ac/pearson/cti/yearplanner/dataaccesslayer/ExcelReaderTest.java
+++ src/test/java/za/ac/pearson/cti/yearplanner/dataaccesslayer/ExcelReaderTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dataaccesslayer;
+package za.ac.pearson.cti.yearplanner.dataaccesslayer;
 
 import org.junit.After;
 import org.junit.Before;
