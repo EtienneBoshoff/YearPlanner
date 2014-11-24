77f99a1da16f5932bbb44fc1e45b6f7614cf475d
EtienneBoshoff <boshoff.etienne@gmail.com>
2014-11-24 08:58:06 +0200

    Added future background images for themes

---
+++ src/main/java/za/ac/pearson/cti/yearplanner/dataaccesslayer/ExcelWriter.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.pearson.cti.yearplanner.dataaccesslayer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
+import java.util.Locale;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+
+/**
+ *
+ * @author UBOSHET
+ */
+public class ExcelWriter {
+    
+    private final File template;
+    
+    private File excelFile;
+
+    public ExcelWriter(File template, File outputFile) {
+        this.template = template;
+        this.excelFile = outputFile;
+    }
+    
+    public ExcelWriter(File template, String absolutePathOfOutputFile) {
+        this.template = template;
+        this.excelFile = new File(absolutePathOfOutputFile);
+    }
+
+    public void setExcelFile(File newFile) {
+        this.excelFile = newFile;
+    }
+    
+    public boolean CreateNewExcelFile() throws IOException, WriteException {
+        if (this.excelFile.exists()) {
+            return false;
+        }
+        WorkbookSettings workbookSettings = new WorkbookSettings();
+        workbookSettings.setLocale(Locale.ENGLISH);
+        WritableWorkbook workbook = Workbook.createWorkbook(excelFile, workbookSettings);
+        workbook.createSheet("Year Planner", 0);
+        workbook.close();
+        return true;
+    }
+    
+    public boolean CreateExcelFileFromTemplate() throws IOException {
+        if (this.excelFile.exists()) {
+            return false;
+        }
+        Files.copy(this.template.toPath(), this.excelFile.toPath(), COPY_ATTRIBUTES);
+        return true;
+    }
+    
+}
